65038ae56aecf95a5106331da74d148a8e22f325
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-29 12:57:59 +0600

    path event modified

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const myPath = 'D:\back-end\1_node.js_basics_raw nodejs';
 console.log(path?.basename(myPath));
 console.log(path?.dirname(myPath));
 console.log(path?.extname(myPath));
-console.log(path?.format(myPath));
+console.log(path?.isAbsolute(myPath));
+console.log(path?.parse(myPath));
+console.log(path?.normalize(myPath));
+console.log(path?.toNamespacedPath(myPath));
