c6748f86789bbd2b2af2c08d026e8d53a11e78ee
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-29 12:53:43 +0600

    path events added

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const myPath = 'D:\back-end\1_node.js_basics_raw nodejs';
+
+console.log(path?.basename(myPath));
+console.log(path?.dirname(myPath));
+console.log(path?.extname(myPath));
+console.log(path?.format(myPath));
