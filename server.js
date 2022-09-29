bf1a8f605324fa7d841f545aa2b4e1ab29d6abc5
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-29 13:11:32 +0600

    os events added

---
+++ server.js
@@ -0,0 +1,9 @@
+const os = require('os');
+
+console.log(os?.EOL);
+console.log(os?.arch());
+console.log(os?.constants);
+console.log(os?.cpus());
+console.log(os?.devNull);
+console.log(os?.homedir);
+console.log(os?.hostname);
