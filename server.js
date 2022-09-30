d3ef45a2ff35c2d15bb50aada1a43f9a450e86dc
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-30 11:58:29 +0600

    read file added

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const fs = require('fs');
 fs.writeFileSync('myFirstFile.txt', 'Dadu!! I LOVE YOOU!!');
 // appeniding to the file
 fs.appendFileSync('myFirstFile.txt', ' and also DADA');
+
+// reading file
+const readData = fs.readFileSync('myFirstFile.txt');
+console.log(readData);
+console.log(readData?.toString());
