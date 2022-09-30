7c431b0d61fc5c8164214f4cacf409d9720d0018
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-30 11:23:33 +0600

    append file added

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const fs = require('fs');
 
 // creating a synchornise file
 fs.writeFileSync('myFirstFile.txt', 'Dadu!! I LOVE YOOU!!');
+// appeniding to the file
+fs.appendFileSync('myFirstFile.txt', ' and also DADA');
