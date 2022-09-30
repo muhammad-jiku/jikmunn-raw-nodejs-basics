059e53587b042512f87c3e84ed9a246fbf83ab21
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-30 22:08:05 +0600

    using parameter as text

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 // register a listener for event
-emitter.on('sayWords', () => {
-  console.log('say a word!! you punk!!');
+emitter.on('sayWords', (word) => {
+  console.log(`say a word!! you punk!! and the word is ${word}`);
 });
 
 // creating a event or raise an event
 // emitter.emit('sayWords');
 setTimeout(() => {
-  emitter.emit('sayWords');
-}, 5600);
+  emitter.emit('sayWords', 'hello sema');
+}, 6600);
