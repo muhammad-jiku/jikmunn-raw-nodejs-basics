2620423940c536eb973415993ee1b7a82072b20c
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-30 12:07:19 +0600

    reading files sychronus or asynchronus wise

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const fs = require('fs');
 // creating a synchornise file
 fs.writeFileSync('myFirstFile.txt', 'Dadu!! I LOVE YOOU!!');
 // appeniding to the file
-fs.appendFileSync('myFirstFile.txt', ' and also DADA');
+fs.appendFileSync('myFirstFile.txt', ' and also you DADA!!');
 
-// reading file
+// reading file synchroniselly
 const readData = fs.readFileSync('myFirstFile.txt');
 console.log(readData);
 console.log(readData?.toString());
+
+// reading file asychorisely
+fs.readFile('myFirstFile.txt', (err, data) => {
+  console.log(data);
+  console.log(data?.toString());
+});
