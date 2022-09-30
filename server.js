abe658c37ffcacc2355b55eb8809f7ec794ac616
muhammad-jiku <muhammadjiku364@gmail.com>
2022-09-30 22:10:38 +0600

    using parameter as object

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const emitter = new EventEmitter();
 
 // register a listener for event
 emitter.on('sayWords', (word) => {
-  console.log(`say a word!! you punk!! and the word is ${word}`);
+  console.log(
+    `say a word!! you punk!! and then ${word?.name} says ${word?.greet}`
+  );
 });
 
 // creating a event or raise an event
 // emitter.emit('sayWords');
 setTimeout(() => {
-  emitter.emit('sayWords', 'hello sema');
+  emitter.emit('sayWords', {
+    greet: 'hello',
+    name: 'sema',
+  });
 }, 6600);
