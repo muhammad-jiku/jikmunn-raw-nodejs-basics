8f69fe682b1d268e8d34939b9c3a874de4341ece
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-01 22:05:15 +0600

    listening to the event

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const server = http.createServer();
 
 const port = process.env.PORT || 5000;
 
-console.log('running http in server');
+server.on('connection', () => {
+  console.log('listening to an event...in http!!');
+});
+
 server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
