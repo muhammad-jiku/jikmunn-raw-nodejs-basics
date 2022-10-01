4462117dccb895fffbf0f1d0e702b0f9184878a4
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-01 21:55:09 +0600

    http added

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Welcome to the lessons of nodeJS built-in modules');
+const http = require('http');
+
+const server = http.createServer();
+
+const port = process.env.PORT || 5000;
+
+console.log('running http in server');
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
