31c2a82645b5d62b53841d3abc12019675408843
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-01 22:20:23 +0600

    response based on requested url or route is added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write(
-    'RYAN: hey, hugh! wanna play WOLVERINE one more time?\nHUGH: yeah! SURE!! ryan!'
-  );
-  res.end();
+  if (req.url === '/') {
+    res.write(
+      'RYAN: hey, hugh! wanna play WOLVERINE one more time?\nHUGH: yeah! SURE!! ryan!'
+    );
+    res.end();
+  } else if (req.url === '/deadpool') {
+    res.write('RYAN: Thank you Kevin Feige!!\nHUGH: Love ya Kevin Feige!');
+    res.end();
+  } else {
+    res.write('DEADPOOL x WOLVERINE');
+    res.end();
+  }
 });
 
 const port = process.env.PORT || 5000;
