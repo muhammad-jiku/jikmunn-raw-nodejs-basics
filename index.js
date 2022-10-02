65604fd1ca9f918225913fceab8e2fe124b74770
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-02 14:45:40 +0600

    pipe for stream and buffer event's shortcut added

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
-
-ourReadStream.on('data', (chunk) => {
-  // every chunk is a buffer
-  console.log(chunk?.toString());
-});
