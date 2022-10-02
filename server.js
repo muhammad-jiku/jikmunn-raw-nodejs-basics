25d20ac90aa33e9e14754e31675daf00993abeb3
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-02 15:02:59 +0600

    sending response via chunk by using pipe

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Welcome to the lessons of nodeJS built-in modules');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const myReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf-8');
+  myReadStream.pipe(res);
+});
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
